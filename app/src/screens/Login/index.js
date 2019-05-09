2427e2c9f40aadcdb8ef2f6e336aa041ae370352
Davidson Edwards <davidson@scape.io>
2019-05-09 16:55:32 +0100

    added base components for login, tests and app colors

--- app/src/screens/Login/index.js
+++ app/src/screens/Login/index.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react'
+import LoginForm from '../../components/LoginForm'
 
 export default class Login extends Component {
     render (){
-        return <div>Login</div>
+        return (
+        <div>
+            <LoginForm />
+        </div>
+        )
     }
 }
