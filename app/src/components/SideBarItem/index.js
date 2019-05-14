03adc06b48bcc690b7e7927d9727b58067914f6a
Davidson Edwards <davidson@scape.io>
2019-05-14 14:00:43 +0100

    added onclick to sidebaritem

--- app/src/components/SideBarItem/index.js
+++ app/src/components/SideBarItem/index.js
@@ -4,9 +4,9 @@ import styles from './SideBarItem.module.css'
 import Icon from '../Icon'
 import {capitalizeFirstLetterOfAllWords} from '../../utils/formatting'
 
-export default ({text="", icon=""}) => {
+export default ({text="", icon="", onClick=()=>{}}) => {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} onClick={onClick}>
             <Icon icon={icon} />
             <span className={styles.text}>{capitalizeFirstLetterOfAllWords(text)}</span>
         </div>
