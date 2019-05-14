ff2e95745386d42919b8a468267009b0843116e9
Davidson Edwards <davidson@scape.io>
2019-05-14 13:41:52 +0100

    added formatting functions from mobile app

--- app/src/components/SideBarItem/index.js
+++ app/src/components/SideBarItem/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from './SideBarItem.module.css'
 
 import Icon from '../Icon'
+import {capitalizeFirstLetterOfAllWords} from '../../utils/formatting'
 
 export default ({text="", icon=""}) => {
     return (
         <div className={styles.container}>
             <Icon icon={icon} />
-            <span className={styles.text}>{text}</span>
+            <span className={styles.text}>{capitalizeFirstLetterOfAllWords(text)}</span>
         </div>
     )
 }
---
+++ app/src/utils/formatting.js
@@ -0,0 +1,39 @@
+import {logError} from "./errorHandling"
+
+export const capitalizeFirstLetter = (string="") => {
+    if (!((typeof string) === "string")) return
+    string = string.toLocaleLowerCase()
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export const numberToOneDecimalPlaceString = number => {
+    if (typeof number === "string") {
+        if(!isNaN(number)) number = Number(number)
+    }
+    if (typeof number !== "number") {
+        logError(`${number} is not a valid number`)
+        return
+    }
+    if (Number.isInteger(number)) return `${number}.0` 
+    const decimalPlaces = number.toString().split(".")[1].length || 0;
+    if (decimalPlaces == 1) return number.toString()
+    else if (decimalPlaces > 1) return `${Math.round(number * 10) / 10}`
+}
+
+export const capitalizeFirstLetterOfAllWords = (string="") => {
+    if (!((typeof string) === "string")) {
+        if (typeof string === "number") string = string.toString()
+        else return
+    }
+    //collapse multiple spaces into one space
+    string = string.replace(/\s\s+/g, ' ');
+    const words = string.split(" ")
+     
+    return words.reduce((capitalizedWords, word) => {
+        word = word.toLocaleLowerCase()
+        word = word.charAt(0).toUpperCase() + word.slice(1);
+        if (capitalizedWords) word = " " + word
+        capitalizedWords = `${capitalizedWords}${word}`
+        return capitalizedWords
+    }, "") 
+}
