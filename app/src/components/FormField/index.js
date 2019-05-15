b5eb4f7c29a5cd77ed5a2e243d83a25cbfd57e4a
Davidson Edwards <davidson@scape.io>
2019-05-15 17:51:44 +0100

    added error text to form fields

--- app/src/components/FormField/index.js
+++ app/src/components/FormField/index.js
@@ -5,6 +5,7 @@ import styles from './FormField.module.css'
 export default ({label="", value="", 
                 name="", icon="", 
                 type='text', customStyles={},
+                error=null,
                 onChange=()=>{}
             }) => {
     
@@ -14,6 +15,7 @@ export default ({label="", value="",
     return (
         <div className={styles.container} style={customStyles}>
             <label className={styles.label}>{label}</label>
+            {error ? <span className={styles.error}>{error}</span> : null }
             <div className={styles.field}>
                 <span className={styles.icon}>{icon? <Icon icon={icon}/> : null}</span> 
                 <input className={styles.input} type={type} value={value} onChange={onChangeText}/>
