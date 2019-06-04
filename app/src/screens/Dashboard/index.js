6ce27353d198dbd846810ff89374221a872e6430
Davidson Edwards <davidson@scape.io>
2019-06-04 13:37:08 +0100

    mocked dashboard

---
+++ app/src/screens/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export class Dashboard extends Component {
+    render (){
+        return <div>hi, ho how do you do</div>
+    }
+}
+
+export default Dashboard 
