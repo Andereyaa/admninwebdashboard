de078a990c3733abe3ad4edbce5a78052bf91583
Davidson Edwards <davidson@scape.io>
2019-06-04 07:00:20 +0100

    introduced shallow rendering

--- app/src/containers/ScreenContent/ScreenContent.test.js
+++ app/src/containers/ScreenContent/ScreenContent.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ScreenContent from './index';
+import ShallowRenderer from 'react-test-renderer/shallow'
+
+import {ScreenContent} from './index';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ScreenContent />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const renderer = new ShallowRenderer()
+  renderer.render(<ScreenContent />);
 });
