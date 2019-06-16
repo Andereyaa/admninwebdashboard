11646b5c1cc3f1b0d1299efbb7b2f51993d86d0c
Davidson Edwards <davidson@scape.io>
2019-06-16 13:40:55 +0100

    added new styling to datepicker

--- app/src/components/DatePicker/index.js
+++ app/src/components/DatePicker/index.js
@@ -7,6 +7,7 @@ export default ({value, onSelect=()=>{}, max=null}) => {
     const dateString = moment(value).format('YYYY-MM-DD')
     const maxDateString = max ?  moment(max).format('YYYY-MM-DD') : null
     return <input 
+                className={styles.container}
                 type="date" 
                 value={dateString}
                 onChange={event => {
