4aa3ebc36facb004c92bffe2131e51f9361d6ee7
Davidson Edwards <davidson@scape.io>
2019-07-01 17:36:54 +0100

    created and styled loading screen

---
+++ app/src/components/LoadingScreen/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "./LoadingScreen.module.css"
+
+export default () => (
+    <div className={styles.container}>
+        <div className={styles.spinner}></div>
+    </div>
+)
