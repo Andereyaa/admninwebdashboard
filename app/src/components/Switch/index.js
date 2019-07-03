bd4ebc472da50d401d0ff2533480f48f7dcd8af5
Davidson Edwards <davidson@scape.io>
2019-07-03 12:23:06 +0100

    added styling to the switch

--- app/src/components/Switch/index.js
+++ app/src/components/Switch/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import styles from './Switch.module.css'
 
 export default ({options=[{text: '', value: ""}]}) => {
+    
     return (
         <div className={styles.container}>
             {
-                options.map(option => <span className={styles.option}>{option.text}</span>)
+                options.map((option, i) => {
+                    const selected = (i > 0)
+                    const selectedStyle = selected ? styles.selected : ""
+                    return <span className={[styles.option, selectedStyle].join(" ")}>{option.text}</span>
+                })
             }
         </div>
     )
