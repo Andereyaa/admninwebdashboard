a67cb573f1f90a53d957ab356c678392a6d7a381
Davidson Edwards <davidson@scape.io>
2019-07-04 17:06:13 +0100

    fixed ordinal number generation

--- app/src/utils/dateHandling.js
+++ app/src/utils/dateHandling.js
@@ -4,12 +4,14 @@ export const getLocalStartOfDayTimestamp = () => new Date().setHours(0, 0, 0, 0)
 
 export const integerToOrdinalNumber = int => {
     if (int < 1 || int > 31) throw new Error("Integer does not correspond to a day in any month")
+    //deal with the teens
+    if (int >= 10 && int <= 20) return `${int}th` 
     const lastDigit = int % 10
     let ending
     switch (lastDigit) {
-        case lastDigit === 1 : ending = "st"; break;
-        case lastDigit === 2 : ending = "nd"; break;
-        case lastDigit === 3 : ending = "rd"; break;
+        case 1: ending = "st"; break;
+        case 2: ending = "nd"; break;
+        case 3 : ending = "rd"; break;
         default: ending = "th"; break;
     }
     return `${int}${ending}`
