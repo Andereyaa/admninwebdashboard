b82eef1e13bd4fa04726c73b8d0a002f62aac0ab
Davidson Edwards <davidson@scape.io>
2019-07-09 10:54:08 +0100

    wrote function to generate periods between two dates

--- app/src/utils/dateHandling.js
+++ app/src/utils/dateHandling.js
@@ -25,10 +25,27 @@ export const findPeriodRangeForDate = timestamp => {
     const periodRange = {}
     if (day <=15) { 
         periodRange.startDate = moment([dateInRange.year(), dateInRange.month(), 1])
-        periodRange.endDate = moment([dateInRange.year(), dateInRange.month(), 15])
+        periodRange.endDate = moment([dateInRange.year(), dateInRange.month(), 15]).endOf('day')
     } else {
         periodRange.startDate = moment([dateInRange.year(), dateInRange.month(), 16])
         periodRange.endDate = moment(dateInRange).endOf('month');
     }
     return periodRange
+}
+
+export const getPeriodsBetweenTwoDatesInclusive = (startDateTimestamp, endDateTimestamp) => {
+    if (!(endDateTimestamp > startDateTimestamp)) throw new Error('The ending timestamp must be greater than the starting timestamp')
+    const startPeriod = findPeriodRangeForDate(startDateTimestamp)
+    const periods = []
+    periods.push(startPeriod)
+    const endPeriod = findPeriodRangeForDate(endDateTimestamp)
+    if (startPeriod.startDate.isSame(endPeriod.startDate, 'day')) return periods //if the start date and end date fall in the same period, return the first period
+    let nextPeriod
+    do {
+        const lastDay = periods[periods.length - 1].endDate
+        const nextDay = lastDay.add('days', 1).startOf('day');
+        nextPeriod = findPeriodRangeForDate(nextDay.valueOf())
+        periods.push(nextPeriod)
+    } while (!(nextPeriod.startDate.isSame(endPeriod.startDate, 'day')))
+    return periods
 }
