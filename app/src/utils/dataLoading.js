3014e84d5c1f5ca4bcca9458578611454c56309a
Davidson Edwards <davidson@scape.io>
2019-07-15 12:26:29 +0100

    implemented 30 second loading for the current period

--- app/src/utils/dataLoading.js
+++ app/src/utils/dataLoading.js
@@ -1,10 +1,15 @@
-import {DAY_IN_MILLISECONDS} from '../constants/time'
+import {DAY_IN_MILLISECONDS, THIRTY_SECONDS_IN_MILLISECONDS} from '../constants/time'
 
 export const shouldLoadMilkCollectionsForCenter = (selectedPeriod, periods, centerId) => {
 
+    //load the milk collections for the center if
     return (
-        (selectedPeriod.id === periods.currentPeriodId) || //if the selected period is the current period
-        !selectedPeriod.dateLoadedByCenterId[centerId] ||
-        selectedPeriod.dateLoadedByCenterId[centerId] < (Date.now() - DAY_IN_MILLISECONDS))
+        !selectedPeriod.dateLoadedByCenterId[centerId] || //the period has never been previously loaded OR
+        (selectedPeriod.dateLoadedByCenterId[centerId] < (Date.now() - DAY_IN_MILLISECONDS)) || //the period was last loaded 24 hours ago OR
+        (
+            (selectedPeriod.id === periods.currentPeriodId) &&//the period is the current period and it was last loaded over 30 seconds ago
+            (selectedPeriod.dateLoadedByCenterId[centerId] < (Date.now() - THIRTY_SECONDS_IN_MILLISECONDS))    
+        )
+    ) 
 
 }
